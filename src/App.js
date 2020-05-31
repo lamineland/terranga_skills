86aacafbbb4dbe82c7a737c3b49fa65d6f43c877
Lamine Sané <aminesane18@gmail.com>
2020-05-31 23:13:13 +0000

    First commi

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TSE - Terranga Skills
         </a>
       </header>
     </div>
